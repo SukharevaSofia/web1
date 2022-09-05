1fb38dd02803e4adc14461f502213e23edb2d294
Sofya <334641@niuitmo.ru>
2022-09-05 12:28:41 +0300

    nothing much changed

---
+++ Web1/lab1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>мяу мяу</title>
+    <link rel="stylesheet" href="general.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="input.css">
+    <link rel="stylesheet" href="area.css">
+  </head>
+
+  
+  <body>
+    <table class="maintable" cellpadding="10" cellspacing="0" width="100%">
+      <tr>
+        <td class="boxheaderl" colspan="1">Вариант 1222</td>
+        <td class="boxheaderr" colspan="1">Сухарева Софья, Р32131</td>
+      </tr>
+      <tr>
+        <td class="area"> 
+          <!--area here-->
+        </td>
+        <td class="inputx">Изменение X:
+          <!-- button -->
+        </td>
+      </tr>
+      <tr>
+        <td class="area"> 
+          <!--area here-->
+        </td>
+
+        <td class="inputy">Изменение Y:
+          <!-- text -->
+        </td>
+      </tr>
+      <tr>
+        <td class="area"> 
+          <!--area here-->
+        </td>
+        <td class="inputz">Изменение Z:
+          <!-- checkbox -->
+        </td>
+        
+      </tr>
+
+    </table>
+  </body>
+</html>
